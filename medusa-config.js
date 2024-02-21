08f3c21de5e38f89d7e6d0df00b04a8a9da7c5c4
sonidiaz <nachodiaz8@gmail.com>
2024-02-21 22:07:11 +0100

    add config only backend

--- medusa-config.js
+++ medusa-config.js
@@ -46,10 +46,11 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
+      // only enable `serve` in development
+      // you may need to add the NODE_ENV variable
+      // manually
+      serve: process.env.NODE_ENV === "development",
+      // other options...
     },
   },
 ];
